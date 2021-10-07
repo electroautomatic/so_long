6d8247b24f121aa3bd6f80574291b973748dea2c
electroautomatic <electroautomatic@gmail.com>
2021-10-07 19:09:03 +0300

    leaks fixed, coin animation work

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: mbalman <mbalman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/02 17:36:59 by mbalman           #+#    #+#             */
-/*   Updated: 2021/10/06 19:42:22 by mbalman          ###   ########.fr       */
+/*   Updated: 2021/10/07 17:52:15 by mbalman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,22 @@
 int	press_key(int key, t_data *data)
 {
 	printf("Hello World! Press key %d\n", key);
-	if (key == 2)
+	if (key == 2 && (data->map.map_pars[(data->player.y)/ 32][(data->player.x)/32 + 1] != '1'))
 		(data->player.x) = (data->player.x) + 32;
-	else if (key == 0)
+	else if (key == 0 && (data->map.map_pars[(data->player.y)/ 32][(data->player.x)/ 32 - 1] != '1'))
 		(data->player.x) = (data->player.x) - 32;
-	else if (key == 13)
+	else if (key == 13 && (data->map.map_pars[(data->player.y)/32 - 1][(data->player.x)/ 32] != '1'))
 		(data->player.y) = (data->player.y) - 32;
-	else if (key == 1)
-		(data->player.y) = (data->player.y) + 32;
+	else if (key == 1 && (data->map.map_pars[(data->player.y)/32 + 1][(data->player.x)/ 32] != '1'))
+		(data->player.y) = (data->player.y) + 32;	
 	else if (key == 53)
+		exit(0);
+	if (data->map.map_pars[(data->player.y)/ 32][(data->player.x)/32] == 'C')
+	{	
+		data->map.map_pars[(data->player.y)/ 32][(data->player.x)/32] = '0';
+		data->coins.count--;
+	}
+	if (data->coins.count == 0 && data->player.x == data->exit.x && data->player.y == data->exit.y)
 		exit(0);
 	return (0);
 }
@@ -76,11 +83,12 @@ void	ft_pars_map(char **argv, t_data *data)
 
 void	ft_map_inint(t_data *data)
 {
-	int	l;
-	int	h;
+	int		l;
+	int		h;
 
 	l = 0;
 	h = 0;
+	data->coins.count = 0;
 	while (h != data->map.map_size_h)
 	{
 		while (l != data->map.map_size_l)
@@ -90,6 +98,13 @@ void	ft_map_inint(t_data *data)
 				data->player.x = l * 32;
 				data->player.y = h * 32;
 			}
+			else if (data->map.map_pars[h][l] == 'E')
+			{
+				data->exit.x = l * 32;
+				data->exit.y = h * 32;
+			}
+			else if (data->map.map_pars[h][l] == 'C')
+				data->coins.count++;
 			l++;
 		}
 		l = 0;
@@ -97,10 +112,58 @@ void	ft_map_inint(t_data *data)
 	}
 }
 
-int	ft_render(t_data *data)
+void	ft_img_inint(t_data *data)
 {
 	int		img_w;
 	int		img_h;
+
+	data->player.img = mlx_xpm_file_to_image(data->mlx_ptr, PLAYER, &img_w, &img_h);	
+	data->map.img_floor = mlx_xpm_file_to_image(data->mlx_ptr, FLOOR, &img_w, &img_h);
+	data->map.img_wall = mlx_xpm_file_to_image(data->mlx_ptr, WALL, &img_w, &img_h);
+	data->coins.img_coin_1 = mlx_xpm_file_to_image(data->mlx_ptr, COIN_1, &img_w, &img_h);
+	data->coins.img_coin_2 = mlx_xpm_file_to_image(data->mlx_ptr, COIN_2, &img_w, &img_h);
+	data->coins.img_coin_3 = mlx_xpm_file_to_image(data->mlx_ptr, COIN_3, &img_w, &img_h);
+	data->coins.img_coin_4 = mlx_xpm_file_to_image(data->mlx_ptr, COIN_4, &img_w, &img_h);
+	data->coins.img_coin_5 = mlx_xpm_file_to_image(data->mlx_ptr, COIN_5, &img_w, &img_h);
+	data->coins.img_coin_6 = mlx_xpm_file_to_image(data->mlx_ptr, COIN_6, &img_w, &img_h);
+	data->coins.img_coin_7 = mlx_xpm_file_to_image(data->mlx_ptr, COIN_7, &img_w, &img_h);
+	data->coins.img_coin_8 = mlx_xpm_file_to_image(data->mlx_ptr, COIN_8, &img_w, &img_h);
+	data->coins.img_coin_9 = mlx_xpm_file_to_image(data->mlx_ptr, COIN_9, &img_w, &img_h);
+	data->coins.img_coin = data->coins.img_coin_5;
+	data->exit.img_exit = mlx_xpm_file_to_image(data->mlx_ptr, EXIT, &img_w, &img_h);
+}
+
+void ft_coin_animation(t_data *data)
+{
+	static int	t;
+
+	if (t < 5)
+		data->coins.img_coin = data->coins.img_coin_1;
+	else if (5 < t && t < 10)
+		data->coins.img_coin = data->coins.img_coin_2;
+	else if (10 < t && t < 15)
+		data->coins.img_coin = data->coins.img_coin_3;
+	else if (15 < t && t < 20)
+		data->coins.img_coin = data->coins.img_coin_4;
+	else if (20 < t && t < 25)
+		data->coins.img_coin = data->coins.img_coin_5;
+	else if (25 < t && t < 30)
+		data->coins.img_coin = data->coins.img_coin_6;
+	else if (30 < t && t < 35)
+		data->coins.img_coin = data->coins.img_coin_7;
+	else if (35 < t && t < 40)
+		data->coins.img_coin = data->coins.img_coin_8;
+	else if (45 < t && t < 50)
+		data->coins.img_coin = data->coins.img_coin_9;
+	t++;
+	if (t == 50)
+	{
+		t = 0;
+	}
+}
+
+int	ft_render(t_data *data)
+{
 	int		l;
 	int		h;
 
@@ -112,19 +175,23 @@ int	ft_render(t_data *data)
 		{
 			if (data->map.map_pars[h][l] != '1')
 			{
-				data->map.img = mlx_xpm_file_to_image(data->mlx_ptr, \
-					FLOOR, &img_w, &img_h);
 				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
-					data->map.img, l * 32, h * 32);
-				//free(data->map.img);
+					data->map.img_floor, l * 32, h * 32);
 			}
 			else if (data->map.map_pars[h][l] == '1')
 			{
-				data->map.img = mlx_xpm_file_to_image(data->mlx_ptr, \
-					WALL, &img_w, &img_h);
 				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
-					data->map.img, l * 32, h * 32);
-				//free(data->map.img);
+					data->map.img_wall, l * 32, h * 32);
+			}
+			if (data->map.map_pars[h][l] == 'C')
+			{
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
+					data->coins.img_coin, l * 32, h * 32);
+			}
+			if (data->map.map_pars[h][l] == 'E' && data->coins.count == 0)
+			{
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, \
+					data->exit.img_exit, l * 32, h * 32);
 			}
 			l++;
 		}
@@ -136,11 +203,6 @@ int	ft_render(t_data *data)
 
 int	ft_player_action(t_data *data)
 {	
-	int		img_width;
-	int		img_height;
-
-	data->player.img = mlx_xpm_file_to_image(data->mlx_ptr, \
-		PLAYER, &img_width, &img_height);
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->player.img, \
 		data->player.x, data->player.y);
 	return (0);
@@ -151,6 +213,7 @@ int	ft_frame(t_data *data)
 	mlx_clear_window(data->mlx_ptr, data->win_ptr);
 	ft_render(data);
 	ft_player_action(data);
+	ft_coin_animation(data);
 	return (0);
 }
 
@@ -158,11 +221,13 @@ int	main(int argc, char **argv)
 {
 	t_data	data;
 
+	data.mlx_ptr = mlx_init();
 	ft_map_volidation(argc, argv, &data);
 	ft_pars_map(argv, &data);
 	ft_map_inint(&data);
-	data.mlx_ptr = mlx_init();
-	data.win_ptr = mlx_new_window(data.mlx_ptr, data.map.map_size_l * 32, data.map.map_size_h * 32, "21 School");
+	ft_img_inint(&data);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, data.map.map_size_l * 32,\
+		data.map.map_size_h * 32, "21 School");
 	mlx_loop_hook(data.mlx_ptr, ft_frame, &data);
 	mlx_key_hook(data.win_ptr, press_key, &data);
 	mlx_clear_window(data.mlx_ptr, data.win_ptr);
--- so_long.h
+++ so_long.h
@@ -9,8 +9,18 @@
 # include "libft/libft.h"
 
 # define	FLOOR 	"s/desert-32.xpm"
-# define	WALL 	"s/wall-32.xpm"
+# define	WALL 	"s/wall.xpm"
 # define	PLAYER 	"s/player_1.xpm"
+# define	COIN_1	"s/coins/Coin1.xpm"
+# define	COIN_2	"s/coins/Coin2.xpm"
+# define	COIN_3	"s/coins/Coin3.xpm"
+# define	COIN_4	"s/coins/Coin4.xpm"
+# define	COIN_5	"s/coins/Coin5.xpm"
+# define	COIN_6	"s/coins/Coin6.xpm"
+# define	COIN_7	"s/coins/Coin7.xpm"
+# define	COIN_8	"s/coins/Coin8.xpm"
+# define	COIN_9	"s/coins/Coin9.xpm"
+# define	EXIT	"s/exit.xpm"
 
 typedef struct s_map
 {
@@ -19,7 +29,8 @@ typedef struct s_map
 	int		map_size_h;
 	char	*img_path_floor;
 	char	*ing_pach_wall;
-	void	*img;
+	void	*img_wall;
+	void	*img_floor;
 }				t_map;
 
 typedef struct s_player
@@ -35,31 +46,42 @@ typedef struct s_player
 // 	int	y;
 // }				t_enemy;
 
-// typedef struct s_coins
-// {
-// 	int	x;
-// 	int	y;
-// }				t_coins;
+typedef struct s_coins
+{
+	// int		x;
+	// int		y;
+	int		count;
+	void	*img_coin;
+	void	*img_coin_1;
+	void	*img_coin_2;
+	void	*img_coin_3;
+	void	*img_coin_4;
+	void	*img_coin_5;
+	void	*img_coin_6;
+	void	*img_coin_7;
+	void	*img_coin_8;
+	void	*img_coin_9;	
+}				t_coins;
 
-// typedef struct s_exit
-// {
-// 	int	x;
-// 	int	y;
-// }				t_exit;
+typedef struct s_exit
+{
+	int		x;
+	int		y;
+	void	*img_exit;
+}				t_exit;
 
 typedef struct s_data
 {
 	t_map		map;
 	t_player	player;
 	// t_enemy		*enmy;
-	// t_exit		*exit;
-	// t_coins		*coins;
+	t_exit		exit;
+	t_coins		coins;
 	void		*mlx_ptr;
 	void		*win_ptr;
 	int			x;
 	int			y;
-}				t_data;
-
+	}			t_data;
 
 int	get_next_line(int fd, char **line);
 
